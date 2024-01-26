0be6777d9816a9dbae1cb10691f6e36be2ca03b5
Raisybear <elias.spycher@gmail.com>
2024-01-26 11:21:11 +0100

    Update server.js

--- public/server.js
+++ public/server.js
@@ -1,24 +1,26 @@
-const express = require('express');
-const multer = require('multer');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
-const upload = multer({ dest: 'public/uploads/' });
+const upload = multer({ dest: "public/uploads/" });
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.post('/upload', upload.single('file'), (req, res) => {
-    const file = req.file;
-    const audioPath = path.join('public', 'music', file.originalname);
-    fs.renameSync(file.path, audioPath);
-    res.send('File uploaded successfully.');
+app.post("/upload", upload.single("file"), (req, res) => {
+  const file = req.file;
+  const audioPath = path.join("public", "music", file.originalname);
+  fs.renameSync(file.path, audioPath);
+  res.send("File uploaded successfully.");
 });
 
 app.listen(3000, () => {
-    console.log('Server listening on port 3000.');
+  console.log(
+    "Server Online, View Website through this link: http://localhost:3000/."
+  );
 });
